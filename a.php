7e48569248926d1f6e4cde41b8759770d5790fca
mango2017 <1210056515@qq.com>
2017-12-18 13:20:32 +0800

    Update a.php

--- a.php
+++ a.php
@@ -1,16 +1,4 @@
 <?php 
 class A{
-    public function b{
-        echo "b";
-    }
-    public function c(){
-        echo "c";
-    }
-    public function d(){
-        echo "d";
-        echo "ddd";
-    }
-    public function e(){
-        echo "edddddsss1111";
-    }
+    
 }
